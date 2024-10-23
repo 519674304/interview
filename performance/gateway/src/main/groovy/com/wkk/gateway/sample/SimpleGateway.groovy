1cb6e519e1bc827475d66ce7b1e190f1d6a0809c
kangkangwu <kangkangwu@fintopia.tech>
2024-10-23 15:35:58 +0800

    修改内容

--- performance/gateway/src/main/groovy/com/wkk/gateway/sample/SimpleGateway.groovy
+++ performance/gateway/src/main/groovy/com/wkk/gateway/sample/SimpleGateway.groovy
@@ -1,14 +1,22 @@
 package com.wkk.gateway.sample
 
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.function.RouterFunction
 import org.springframework.web.servlet.function.RouterFunctions
 import org.springframework.web.servlet.function.ServerResponse
 
+@Configuration
 class SimpleGateway {
-  @Bean
+  //@Bean
   RouterFunction<ServerResponse> getRoute() {
-    RouterFunctions.route().GET("/get", HandlerFunctions.http("https://httpbin.org")).build();
+    RouterFunctions.route().GET("/get", HandlerFunctions.http("https://httpbin.org/")).build()
+  }
+
+  @Bean
+  RouterFunction<ServerResponse> getSimpleRoute() {
+    GatewayRouterFunctions.route("simple_route").GET ("/get", HandlerFunctions.http("https://httpbin.org/")).build()
   }
 }

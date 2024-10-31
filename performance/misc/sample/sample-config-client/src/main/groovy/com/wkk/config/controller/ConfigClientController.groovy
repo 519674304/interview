07882e1bc4c4a138ac071c76ae6ac4ee32af1650
wukangkang <519674304@qq.com>
2024-10-31 20:06:09 +0800

    增加config client

--- performance/misc/sample/sample-config-client/src/main/groovy/com/wkk/config/controller/ConfigClientController.groovy
+++ performance/misc/sample/sample-config-client/src/main/groovy/com/wkk/config/controller/ConfigClientController.groovy
@@ -1,5 +1,6 @@
 package com.wkk.config.controller
 
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.context.config.annotation.RefreshScope
@@ -19,6 +20,15 @@ class ConfigClientController {
     @GetMapping("/getPort")
     String getPort(){
         return "port:${port},serverPort:${serverPort}"
+    }
+
+    @Test
+    public void test25(){
+        for (i in 0..<60) {
+            if ((12 * i) % 60 == i) {
+                println i
+            }
+        }
     }
 
 }

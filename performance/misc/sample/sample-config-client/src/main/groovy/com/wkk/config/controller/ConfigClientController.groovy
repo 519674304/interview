08dd64d6e77e45a707e0a42a9792d3ad12918337
wukangkang <519674304@qq.com>
2024-10-31 18:48:43 +0800

    增加config client

--- performance/misc/sample/sample-config-client/src/main/groovy/com/wkk/config/controller/ConfigClientController.groovy
+++ performance/misc/sample/sample-config-client/src/main/groovy/com/wkk/config/controller/ConfigClientController.groovy
@@ -1,10 +1,14 @@
 package com.wkk.config.controller
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.context.config.annotation.RefreshScope
+import org.springframework.cloud.context.refresh.ContextRefresher
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RefreshScope
 class ConfigClientController {
     @Value('${port}')
     private String port
